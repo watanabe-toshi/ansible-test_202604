fbd781a0eacf988736e4112a4c810acb3e20fddd
watanabe-toshi <109321773+watanabe-toshi@users.noreply.github.com>
2026-04-02 18:13:50 +0900

    Add outputs for VPC and Windows instance details

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,17 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
 output "linux_public_ip" {
   value = aws_instance.amazon_linux.public_ip
 }
 
 output "linux_private_ip" {
   value = aws_instance.amazon_linux.private_ip
+}
+
+output "linux_instance_id" {
+  value = aws_instance.amazon_linux.id
 }
 
 output "windows_public_ip" {
@@ -16,4 +24,8 @@ output "windows_private_ip" {
 
 output "windows_instance_id" {
   value = aws_instance.windows.id
+}
+
+output "windows_admin_user" {
+  value = "Administrator"
 }
